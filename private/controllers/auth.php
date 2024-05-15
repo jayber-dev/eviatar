b363039e77a77aad4971fbed907dabad182af2c6
jayber <jayber1@gmail.com>
2024-05-15 10:12:59 +0300

    AUTH changed

--- private/controllers/auth.php
+++ private/controllers/auth.php
@@ -13,7 +13,26 @@ class Auth
     }
 
     function login() {
-        
+        include "../utilities/connect.php";
+        if(isset($_POST['username'])){
+            $userName = $_POST['username'];
+            $password = $_POST['password'];
+
+            $sql = "SELECT * FROM users WHERE user_name = '$userName' and password = '$password'";
+            $result = mysqli_query($mysqli, $sql);
+            $row = mysqli_num_rows($result);
+            if($row == 1){
+                $name_rows = mysqli_fetch_assoc($result);
+                $_SESSION['name'] = $name_rows['first_name'];
+                $_SESSION['admin'] = $name_rows['is_admin'];
+                
+                header("Location: /store/index.php");
+            }
+            else{
+                echo "Wrong password";
+            }
+        }
     }
    
 }
+
--- views/login.php
+++ views/login.php
@@ -13,29 +13,13 @@
 
         session_start();
         include "../utilities/connect.php";
-        
+        require_once('../private/controllers/auth.php');
         ?>
         <form action="/store/views/login.php" method="post">
             <div class="error-message">
-                <?php
-                if(isset($_POST['username'])){
-                    $userName = $_POST['username'];
-                    $password = $_POST['password'];
-
-                    $sql = "SELECT * FROM users WHERE user_name = '$userName' and password = '$password'";
-                    $result = mysqli_query($mysqli, $sql);
-                    $row = mysqli_num_rows($result);
-                    if($row == 1){
-                        $name_rows = mysqli_fetch_assoc($result);
-                        $_SESSION['name'] = $name_rows['first_name'];
-                        $_SESSION['admin'] = $name_rows['is_admin'];
-                        
-                        header("Location: /store/index.php");
-                    }
-                    else{
-                        echo "Wrong password";
-                    }
-                }
+                <?php 
+                    $login = new Auth();
+                    $login->login();
                 ?>
             </div>
             <label for="username">Username:</label>
